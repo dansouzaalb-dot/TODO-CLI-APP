53c1f5798afeeeea577e5c47a436f807784fe850
Daniel <dan.souza.alb@gmail.com>
2026-04-22 22:15:57 -0300

    Added .gitignore

--- storage.py
+++ storage.py
@@ -4,7 +4,7 @@ from validation import valid_date, valid_priority, valid_index
 from color import red, green
 from task import Task
 
-FILE = "tasks.json"
+FILE = "data/tasks.json"
 
 
 #--- Load Tasks ---
